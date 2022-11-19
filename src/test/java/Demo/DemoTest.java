f6b857b7e3eca1b929ed157d152c88e8675a4ec8
krishnavk0302 <krishnavk0302@gmail.com>
2022-11-19 20:02:01 +0530

    commit 1

---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demo()
+	{
+		System.out.println("hi this is wasm6 framework!!!");
+		System.out.println("hi");
+	}
+
+}
